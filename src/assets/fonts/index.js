80c03d909c7267a11d501feb1447606f9fdf31bc
Aleksandra Kurek <kurek.aleksandra@gmail.com>
2019-10-21 20:07:44 +0200

    Add styles; add utils

---
+++ src/assets/fonts/index.js
@@ -0,0 +1,13 @@
+import BebasNeueTTF from "./BebasNeue-Regular.ttf"
+import BebasNeueOTF from "./BebasNeue-Regular.otf"
+import BebasNeueWOFF from "./BebasNeue-Regular.woff"
+import BebasNeueWOFF2 from "./BebasNeue-Regular.woff2"
+import Avenir from "./AvenirRoman.otf"
+
+export const fonts = {
+  BebasNeueTTF,
+  BebasNeueOTF,
+  BebasNeueWOFF,
+  BebasNeueWOFF2,
+  Avenir,
+}
---
+++ src/config.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export const colors = {
+  darkBlueGrey: '#13243e',
+  darkBlack: 'rgba(0, 0, 0, 0.85)',
+  charcoalGrey: '#293134',
+  silver: '#c0c8cd',
+  slate: '#57677b',
+  blueyGrey: '#899aac',
+  veryLightBlue: '#daeeff',
+  azure: '#0087fd',
+  azul: '#1665d8',
+  brightBlue: '#0078ff',
+  brightBlueTwo: '#0075fe',
+  electricBlue: '#0149ff',
+  dodgerBlue: '#4f81fc',
+
+  white: '#ffffff',
+  red: '#ff0000',
+  yellow: '#ffD769',
+  whiteGrey: '#f9f9f9',
+  paleGrey: '#e2e5ed',
+  iceBlue: '#f7faff',
+  blueGrey: '#899aac',
+  coolGrey: '#9ea0a5',
+  fadeWarmGrey: 'rgba(151, 151, 151, 0.5)',
+  darkBlueGreyTwo: '#1b2c42',
+  darkBlueGreyThree: '#14243c',
+  darkBlueGreyFour: '#1d2e44',
+  black: '#333333',
+  darkBlueGreyFive: '#223346',
+  battleShipGrey: '#677387',
+
+};
+
+export const colorsRGBA = {
+  fadeDodgerBlue: 'rgba(41, 173, 255, 0.05)',
+  fadeBlueGrey: 'rgba(139, 155, 172, 0.5)',
+  fadeAzure: 'rgba(0, 135, 253, 0.2)',
+  fadeWhite: 'rgba(255, 255, 255, 0.06)',
+  fadeGrey: 'rgba(0, 0, 0, 0.2)',
+
+};
+
+export const shadows = {
+  baseBoxShadow: '0 30px 40px 0 rgba(9,6,65,.03)',
+};
